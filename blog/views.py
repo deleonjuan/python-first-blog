1b3aa22f3e94315bb77e13db3e4b2b34aa09fac7
Juan Alberto de Leon <juannillo95@gmail.com>
2020-09-27 23:22:21 -0600

    Mi aplicación Django Girls, primer commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 
-# Create your views here.
+
+def publicacion_lista(request):
+    publicaciones = Publicacion.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/publicacion_lista.html', {'publicaciones':publicaciones})
